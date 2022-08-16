25221b960daf844bdb7f1af8f866342f9f5d4549
Antton Lamarca <anttonlamarca@gmail.com>
2022-08-16 16:52:11 +0200

    Add ldlink tests

--- Tests/helpers/test_ldlink.py
+++ Tests/helpers/test_ldlink.py
@@ -1,17 +1,22 @@
 from helpers.ldlink import *
 
+"""
+Testing for the LDLink related functions. Note that these functions are quite slow to run because they need to connect
+to the LDLink API.
+"""
+
 
 def test_ldproxy() -> None:
     """
     Test that the dataframe is created and is not empty.
     """
-    df = ldproxy('rs333')
+    df = ldproxy('rs16886165')
     assert not df.empty
 
 
-def test_ldtrait():
+def test_ldtrait() -> None:
     """
-    Test if LDtrait works.
+    Test that the dataframe is created and is not empty.
     """
-    df = ldproxy('rs333')
+    df = ldtrait('rs16886165')
     assert not df.empty
