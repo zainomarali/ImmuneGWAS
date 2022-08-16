5f451531f641ee78d00730c36c40c29e8ed4ca01
Antton Lamarca <anttonlamarca@gmail.com>
2022-08-16 17:40:58 +0200

    Add column tests

--- Tests/helpers/test_ldlink.py
+++ Tests/helpers/test_ldlink.py
@@ -1,22 +1,48 @@
+import pytest
 from helpers.ldlink import *
 
 """
 Testing for the LDLink related functions. Note that these functions are quite slow to run because they need to connect
 to the LDLink API.
+We use the SNP rs16886165 because we know it has output for both LDproxy and LDtrait. This could in the future, however,
+and a reason for the tests failing could be the the LDlink database itself has changed!
 """
 
 
-def test_ldproxy() -> None:
+@pytest.fixture
+def ldproxy_df():
+    return ldproxy('rs16886165')
+
+
+@pytest.fixture
+def ldtrait_df():
+    return ldtrait('rs16886165')
+
+
+def test_ldproxy(ldproxy_df) -> None:
     """
     Test that the dataframe is created and is not empty.
     """
-    df = ldproxy('rs16886165')
-    assert not df.empty
+    assert not ldproxy_df.empty
 
 
-def test_ldtrait() -> None:
+def test_ldproxy_columns(ldproxy_df) -> None:
+    """
+    Test that the dataframe has the necessary columns
+    """
+    assert "R2" in ldproxy_df.columns
+
+
+def test_ldtrait(ldtrait_df) -> None:
     """
     Test that the dataframe is created and is not empty.
     """
-    df = ldtrait('rs16886165')
-    assert not df.empty
+    assert not ldtrait_df.empty
+
+
+def test_ldtrait_columns(ldtrait_df) -> None:
+    """
+    Test that the dataframe has the necessary columns.
+    TODO: Complete this test
+    """
+    assert False
