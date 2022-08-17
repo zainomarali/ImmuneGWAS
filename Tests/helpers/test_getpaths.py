ce563049779fb7578d240c13c85e1df0707136d4
AnttonLA <anttonlamarca@gmail.com>
2022-08-17 17:39:51 +0200

    Reformat getpaths.py. Add test script for it

---
+++ Tests/helpers/test_getpaths.py
@@ -0,0 +1,17 @@
+from helpers.getpaths import *
+
+
+def test_get_paths():
+    """
+    Test that the file paths are correct. Beware that the filepaths might be changed in cbio3, making this test useless.
+    """
+    p_dict = get_paths("/path/to/dir/")
+
+    assert p_dict['eqtl_cat'] == "/path/to/dir/cbio3/data/eQTL_DB/"
+    assert p_dict['dbsnp'] == "/path/to/dir/cbio3/data/dbSNP/"
+    assert p_dict['eqtl_tokyo'] == "/path/to/dir/cbio3/projects/Zain_2021/ImmuNEXT_Japan_Cell2021/eQTL_summarystats/"
+    assert p_dict['ge_tokyo'] == "/path/to/dir/cbio3/projects/Zain_2021/ImmunexUT_GE/E-GEAD-397.processed/tpm/"
+    assert p_dict['eqtlgen_cis'] == "/path/to/dir/cbio3/projects/Zain_2021/eQTLgen/data/2019-12-11-cis-eQTLsFDR0.05"\
+                                    "-ProbeLevel-CohortInfoRemoved-BonferroniAdded_sorted.txt "
+    assert p_dict['eqtlgen_trans'] == "path/to/dir/cbio3/projects/Zain_2021/eQTLgen/data/2018-09-04-trans-eQTLsFDR0.05"\
+                                      "-CohortInfoRemoved-BonferroniAdded.txt "
