3e897a88376f078ea03e891350682f0a0ec85345
AnttonLA <anttonlamarca@gmail.com>
2022-08-18 12:09:57 +0200

    Move Variant out of prototyping

--- Tests/test_Variant.py
+++ Tests/test_Variant.py
@@ -1,5 +1,17 @@
+import Variant
+
+
+# TODO: When the code gets moved out of 'prototyping', this test should load the correct class.
+
 def test_from_rsid():
-    assert False
+    input_rsid = "rs9438875"
+    var = Variant.from_rsid(input_rsid)
+
+    assert var.get_rsid() == input_rsid
+    assert var.get_chrom() == "1"
+    assert var.get_pos() == "25231562"
+    assert var.get_ea() == "T"
+    assert var.get_oa() == "G"
 
 
 def test_get_rsid():
