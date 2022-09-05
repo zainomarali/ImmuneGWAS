8a704e427506310c6ae894689ba60dc70b5a7985
Antton Lamarca <35562017+AnttonLA@users.noreply.github.com>
2022-09-05 13:58:52 +0200

    Change defautl config.py contents

--- ImmuneGWAS/config.py
+++ ImmuneGWAS/config.py
@@ -1,12 +1,12 @@
 import logging
 
-cbio_root = "/home/antton/"  # Path to cbio3
-output_folder = "/home/antton/Desktop/ImmuneGWAS_post_output/"  # Destination for output files
+cbio_root = "/default/path/to/cbio/"  # Path to cbio3
+output_folder = "/default/output/folder/path/"  # Destination for output files
 
 if cbio_root == "/default/path/to/cbio/":
     raise ValueError("Default path to cbio3 is used. Please change this in config.py")
 
-if output_folder == "/default/path/to/output/folder/":
+if output_folder == "/default/output/folder/path/":
     raise ValueError("Default path to output folder is used. Please change this in config.py")
 
 # Initialize logging
