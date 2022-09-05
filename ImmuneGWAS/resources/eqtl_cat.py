f4a75abcaaff7998c5ece432a43a9232b5d36689
zain <zainomarali@gmail.com>
2022-09-05 17:06:00 +0200

    changed some trans stuff

--- ImmuneGWAS/resources/eqtl_cat.py
+++ ImmuneGWAS/resources/eqtl_cat.py
@@ -106,7 +106,7 @@ def single_eqtl_catalogue_query_type_restricted(chromosome: int, position: int, 
             if EA == concatenated_df["alt"].iloc[0]:
                 pass
             elif EA == concatenated_df["ref"].iloc[0]:
-                logging.info(f"LDblock EA {EA} corresponds to eQTLcat OA {concatenated_df['OA'].iloc[0]}. "
+                logging.info(f"LDblock EA {EA} corresponds to eQTLcat OA {concatenated_df['ref'].iloc[0]}. "
                              f"Sign of the z value flipped.")
                 concatenated_df.z = concatenated_df.z * -1
             elif concatenated_df["alt"].iloc[0][0] == concatenated_df["ref"].iloc[0][0]:  # Deletion/addition case
